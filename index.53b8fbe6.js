ef742928a75bf529e7d37627f698cb82550288c0
desholmes <desholmes@gmail.com>
2021-12-04 16:56:24 +0000

    Updating site Sat  4 Dec 2021 16:56:24 GMT

--- index.53b8fbe6.js
+++
@@ -1,2 +0,0 @@
-const t={containerId:"tfft-countdown",targetDate:{date:23,month:11,year:2021}},e=()=>{new TfftCountdown(t)};"complete"===document.readyState||"loading"!==document.readyState&&!document.documentElement.doScroll?e():document.addEventListener("DOMContentLoaded",e);
-//# sourceMappingURL=index.53b8fbe6.js.map
